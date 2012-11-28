6476e2af8f4636cc2419d7289037ab4c50ce0390
josh <josh@6d5b3bcb-2a12-441e-a289-d78745717ac4>
2012-11-28 06:48:59 +0000

    Back to normal functionality (except FastParser doesn't return anything).

--- Snail/Snail.App/MainWindow.xaml.cs
+++ Snail/Snail.App/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Snail;
+using Snail.Nodes;
 
 namespace Snail.App
 {
@@ -31,19 +32,19 @@ namespace Snail.App
 			//string testFile = @"..\Snail.Test\ot.xml";
 			//string testFile = @"..\Snail.Test\TO_core_last_DEM_AR_forests.shp.afe";
 			//string testFile = @"..\Snail.Test\xhtml1-strict.dtd";
-			string testText = File.ReadAllText(testFile);
+			string text = File.ReadAllText(testFile);
 
 			var sw = Stopwatch.StartNew();
 
-			int count = 0;
+			DocumentNode root = null;
 			for (int i = 0; i < 1; i++)
 			{
-				count = Document.Parse2(testText);
+				root = Document.Parse(text);
 			}
 
 			sw.Stop();
 
-			var status = string.Format("Parsed {0} tags in {1} ms", count, sw.Elapsed.TotalMilliseconds);
+			var status = string.Format("Parsed {0} tags in {1} ms", (root != null) ? 1 : 0, sw.Elapsed.TotalMilliseconds);
 			Trace.WriteLine(status);
 			textBlock.Text = status;
 		}
