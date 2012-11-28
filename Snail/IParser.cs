6efb2aa53cd135f2244cbf2a6ef65ee13c607e4e
Joshua Morey <jdauie@users.noreply.github.com>
2012-11-28 06:48:59 +0000

    Back to normal functionality (except FastParser doesn't return anything).

--- Snail/IParser.cs
+++ Snail/IParser.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Snail.Nodes;
+
 namespace Snail
 {
 	interface IParser
 	{
+		DocumentNode Parse(string text);
 	}
 }
---
+++ Snail/LegacyParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Snail.Legacy;
+using Snail.Nodes;
+
+namespace Snail
+{
+	class LegacyParser : IParser
+	{
+		public DocumentNode Parse(string text)
+		{
+			var tokens = Lexer.Tokenize(text);
+			var tags = Parser.ParseTokens(tokens);
+			Parser.CalculateTagDepths(tokens, tags);
+			var tree = Parser.ParseTagsToTree(tokens, tags);
+
+			//string text1 = tree.ToFormattedString();
+			//string text2 = tree.ToFormattedString(WhitespaceMode.Strip);
+			//string text3 = tree.ToFormattedString(WhitespaceMode.Insert, "\t");
+
+			//string testFile = @"\projects\Snail\test1.htm";
+			//File.WriteAllText(testFile, text2);
+
+			return tree;
+		}
+	}
+}
