5772c99bc211be533800ddf25f6c8db67e8652b9
josh <josh@6d5b3bcb-2a12-441e-a289-d78745717ac4>
2012-11-29 06:09:18 +0000

    Added CompareProjectFilesTest()

--- Snail/Snail.App/MainWindow.xaml.cs
+++ Snail/Snail.App/MainWindow.xaml.cs
@@ -28,14 +28,20 @@ namespace Snail.App
 		{
 			InitializeComponent();
 
+			//ParseTest();
+			CompareProjectFilesTest();
+		}
+
+		private void ParseTest()
+		{
 			string testFile;
-			//testFile = @"..\Snail.Test\PsyStarcraft_channel.htm";
+			testFile = @"..\Snail.Test\PsyStarcraft_channel.htm";
 			//testFile = @"..\Snail.Test\kjv-osis\kjv.osis.xml";
 			//testFile = @"..\Snail.Test\kjv-usfx\eng-kjv_usfx.xml";
 			//testFile = @"..\Snail.Test\ot.xml";
 			//testFile = @"..\Snail.Test\TO_core_last_DEM_AR_forests.shp.afe";
 			//testFile = @"..\Snail.Test\xhtml1-strict.dtd";
-			testFile = @"..\Snail.Test\Viewer3D.101.vcproj";
+			//testFile = @"..\Snail.Test\Viewer3D.101.vcproj";
 			string text = File.ReadAllText(testFile);
 
 			var sw = Stopwatch.StartNew();
@@ -49,6 +55,32 @@ namespace Snail.App
 			sw.Stop();
 
 			var status = string.Format("Parsed {0} tags in {1} ms", (root != null) ? 1 : 0, sw.Elapsed.TotalMilliseconds);
+			Trace.WriteLine(status);
+			textBlock.Text = status;
+		}
+
+		private void CompareProjectFilesTest()
+		{
+			CompareProjectFiles(
+				@"..\Snail.Test\Viewer3D.101.vcproj",
+				@"..\Snail.Test\Viewer3D.102.vcproj"
+				);
+		}
+
+		private void CompareProjectFiles(string p1, string p2)
+		{
+			var text1 = File.ReadAllText(p1);
+			var root1 = Document.Parse(text1);
+			//File.WriteAllText(@"..\Snail.Test\compare1.txt", root1.ToFormattedString(WhitespaceMode.Insert, "\t"));
+
+			var text2 = File.ReadAllText(p2);
+			var root2 = Document.Parse(text2);
+			//File.WriteAllText(@"..\Snail.Test\compare2.txt", root2.ToFormattedString(WhitespaceMode.Insert, "\t"));
+
+			// compare trees for differences (not considering order)
+
+
+			var status = string.Format("Compared {0}, {1}", System.IO.Path.GetFileName(p1), System.IO.Path.GetFileName(p2));
 			Trace.WriteLine(status);
 			textBlock.Text = status;
 		}
