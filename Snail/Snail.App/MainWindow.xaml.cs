4a75a6cfff18ec97f4e8dacea6fecc1ee83d2acf
josh <josh@6d5b3bcb-2a12-441e-a289-d78745717ac4>
2012-12-05 06:44:59 +0000

    Switched attribute storage mode back to List<KeyValuePair> and added ref to new test file.

--- Snail/Snail.App/MainWindow.xaml.cs
+++ Snail/Snail.App/MainWindow.xaml.cs
@@ -40,6 +40,7 @@ namespace Snail.App
 			//testFile = @"..\Snail.Test\kjv-usfx\eng-kjv_usfx.xml";
 			//testFile = @"..\Snail.Test\ot.xml";
 			//testFile = @"..\Snail.Test\TO_core_last_DEM_AR_forests.shp.afe";
+			//testFile = @"..\Snail.Test\Precompaction_Lrn.shp.afe";
 			//testFile = @"..\Snail.Test\xhtml1-strict.dtd";
 			//testFile = @"..\Snail.Test\Viewer3D.101.vcproj";
 			string text = File.ReadAllText(testFile);
