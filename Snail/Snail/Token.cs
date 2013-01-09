cacf073bf14c3668a73a2c2152f4f56ddd10f660
josh <josh@6d5b3bcb-2a12-441e-a289-d78745717ac4>
2013-01-09 06:42:59 +0000

    Adding comments.

--- Snail/Snail/Token.cs
+++ Snail/Snail/Token.cs
@@ -4,6 +4,54 @@ using System.Text;
 
 namespace Snail
 {
+	public interface IToken
+	{
+		int Index { get; }
+	}
+
+	/// <summary>
+	/// token    = [         64        ]
+	///          = [  30  ][  4  ][ 30 ]
+	///             index   type   @1
+	/// 
+	/// @1(attr) = [  11  ][   9   ][  10  ]
+	///             qname   prefix   value
+	/// 
+	/// @1(node) = [ 22 ][  8  ]
+	///             @2   depth
+	/// 
+	/// @2(decl),
+	/// @2(tag)  = [  11  ][   9   ][  2  ]
+	///             qname   prefix    ?
+	/// 
+	/// @2(proc) = [  9  ][   13   ]
+	///             target  content
+	/// 
+	/// @2(comment),
+	/// @2(cdata),
+	/// @2(text) = [  22  ]
+	///             length
+	/// </summary>
+	public struct Token2 : IToken
+	{
+		private readonly TokenList m_list;
+		private readonly int m_index;
+		private readonly TokenType m_type;
+
+		public Token2(TokenList list, int index, TokenType type)
+		{
+			m_list = list;
+			m_index = index;
+			m_type = type;
+		}
+
+		public int Index
+		{
+			get { return m_index; }
+		}
+	}
+
+
 	public struct Token
 	{
 		private readonly TokenList m_list;
