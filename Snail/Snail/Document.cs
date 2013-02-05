39d9539df4bd7adeb4f2392169c0c259326085a0
josh <josh@6d5b3bcb-2a12-441e-a289-d78745717ac4>
2013-02-05 05:09:57 +0000

    Testing performance against .NET XML options.
    2.5x faster than XmlTextReader
    10x faster than XmlDocument

--- Snail/Snail/Document.cs
+++ Snail/Snail/Document.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-
+using System.Xml;
 using Snail.Nodes;
 
 namespace Snail
@@ -23,6 +23,29 @@ namespace Snail
 
 		public static TokenList ParseXml(string text)
 		{
+			//var doc = new XmlDocument();
+			//doc.LoadXml(text);
+
+			//int count = 0;
+			//using (var sr = new StringReader(text))
+			//using (var reader = new XmlTextReader(sr))
+			//{
+			//    while (reader.Read())
+			//    {
+			//        if (reader.NodeType == XmlNodeType.Element)
+			//        {
+			//            while (reader.MoveToNextAttribute())
+			//            {
+			//                ++count;
+			//            }
+			//        }
+
+			//        ++count;
+			//    }
+			//}
+
+			//return null;
+
 			var parser = new XmlParser();
 			return parser.Parse(text);
 		}
